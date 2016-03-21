e0f539169efb6d160125d271fe89b37e97b3f993
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2016-03-21 14:23:53 +0100

    add indonesian translation

--- tests/test.js
+++ tests/test.js
@@ -5,6 +5,7 @@ describe('translations', function() {
   it('should include languages', function() {
     assert.isObject(translations.en);
     assert.isObject(translations.cs);
+    assert.isObject(translations.id);
   });
   it('should include keys', function() {
     assert.isString(translations.en.HOW_LIKELY);
